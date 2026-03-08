37932370a82508d9196f788dd102629ed4ce84c6
Liam Cooper <liamcoop@outlook.com>
2026-03-08 14:29:09 -0700

    update to use networkadapter

--- lib/automerge.ts
+++ lib/automerge.ts
@@ -1,8 +1,17 @@
-import { Repo } from "@automerge/automerge-repo";
+import { Repo, type NetworkAdapter } from "@automerge/automerge-repo";
 import { BroadcastChannelNetworkAdapter } from "@automerge/automerge-repo-network-broadcastchannel";
+import { BrowserWebSocketClientAdapter } from "@automerge/automerge-repo-network-websocket";
 import { IndexedDBStorageAdapter } from "@automerge/automerge-repo-storage-indexeddb";
+
+const network: NetworkAdapter[] = [new BroadcastChannelNetworkAdapter()];
+
+if (process.env.NEXT_PUBLIC_SYNC_SERVER_URL) {
+  network.push(
+    new BrowserWebSocketClientAdapter(process.env.NEXT_PUBLIC_SYNC_SERVER_URL)
+  );
+}
 
 export const repo = new Repo({
   storage: new IndexedDBStorageAdapter(),
-  network: [new BroadcastChannelNetworkAdapter()],
+  network,
 });
